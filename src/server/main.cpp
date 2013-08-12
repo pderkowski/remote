97cbbf57f7a8791d9932eaa332ee69f0cfe10ab1
Piotr <piotder@gmail.com>
2013-08-13 00:14:48 +0200

    Server prototype is listening

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <exception>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 
 int main() {
-  std::cout << "Hello world!";
+  try
+  {
+    boost::asio::io_service io_service;
+
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 12345));
+
+    for (;;)
+    {
+      tcp::socket socket(io_service);
+      acceptor.accept(socket);
+    }
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+
+  std::cout << "Hello world!" << std::endl;
   return 0;
 }
+
+
+
+
